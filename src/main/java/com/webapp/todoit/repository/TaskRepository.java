a767fd5f768f2ef3113fd00911945925553c9e26
Abdo-develloper-Keraoui <abdottawabkeraoui@gmail.com>
2025-07-11 17:01:24 +0100

    Improved query method to TaskRepository to fetch tasks by status excluding deleted ones

--- src/main/java/com/webapp/todoit/repository/TaskRepository.java
+++ src/main/java/com/webapp/todoit/repository/TaskRepository.java
@@ -3,6 +3,8 @@ package com.webapp.todoit.repository;
 import com.webapp.todoit.entity.Task;
 import com.webapp.todoit.entity.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,14 +19,14 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     // Spring automatically creates these methods:
     // save(), findById(), findAll(), deleteById()
 
-    /// Get task by its status for display
-    List<Task> findByStatus(TaskStatus status);
+    /// Get task by its status for display and it should not be deleted
+    @Query("SELECT t FROM Task t WHERE t.status = :status AND t.deleted = false")
+    List<Task> findByStatusAndNotDeleted(@Param("status")TaskStatus status);
 
     /// Get tasks that are soft-deleted: deleted == true
     List<Task> findByDeletedIsTrue();
 
     /// Fin non deleted tasks for all view
     List<Task> findByDeletedIsFalseOrderByUpdatedAt();
-
 
 }
